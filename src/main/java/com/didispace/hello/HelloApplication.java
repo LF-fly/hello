a2cfd925bcb0d3392c1279b7e85dbc77e313541b
liufei <1254724027@qq.com>
2018-08-21 14:27:23 +0800

    fix:修改版本号为 1.5.3,并修改相应的pom文件，原因是之前的 1.3.7 版本注册不到服务中心

--- src/main/java/com/didispace/hello/HelloApplication.java
+++ src/main/java/com/didispace/hello/HelloApplication.java
@@ -2,7 +2,10 @@ package com.didispace.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient// 启用 Eureka
 @SpringBootApplication
 public class HelloApplication {
 
